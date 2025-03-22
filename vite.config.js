e695979f84e576db288cfd7e9b3b3b3da821b1cc
SachinKiragi <sachinkiragi8@gmail.com>
2025-03-22 17:46:59 +0530

    add static.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Ensures correct path resolution
+  server: {
+    port: 3000, // Optional: Define your local dev port
+  }
+});
